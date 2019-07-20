668fc9659aa27a9861ae9c76acd74b8bbcd67689
Raul Sposito <raulsposito@gmail.com>
2019-07-20 21:35:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ reverse_each_word("Hello there, and how are you?")
 def reverse_each_word(string)
   array = string.split(" ") 
   test_array = []
-  array.collect do|string| 
+  array.collect do |string| 
     test_array << string.reverse 
   end
   test_array.join(" ")
