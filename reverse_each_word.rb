35c6fa4d23700951d02c2161fe283dfa6c8ccc1c
Raul Sposito <raulsposito@gmail.com>
2019-07-20 21:41:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
-reverse_each_word("Hello there, and how are you?")
 
-def reverse_each_word_with_each(string)
+def reverse_each_word_with_each(string = "Hello there, and how are you?")
   original_array = string.split(" ")
   return_array = []
   original_array.each do|string|
@@ -10,7 +9,7 @@ def reverse_each_word_with_each(string)
 end
 
 
-def reverse_each_word(string)
+def reverse_each_word(string = "Hello there, and how are you?")
   array = string.split(" ") 
   test_array = []
   array.collect do |string| 
