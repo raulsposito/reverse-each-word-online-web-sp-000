b794a174eb5e159bcf1422d7862ddb152131129c
Raul Sposito <raulsposito@gmail.com>
2019-07-20 21:33:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,11 @@ def reverse_each_word(string)
   string.split.reverse.join(' ')
 end
 
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
